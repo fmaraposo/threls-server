b63a809899fbcfe1a818185e2ddb46ac33340f4a
Francisco Martins <fmaraposo@gmail.com>
2023-06-21 22:24:19 +0100

    Initial Commit and set up database connection

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { mongoUri } = require("./config");
+
+const app = express() // start express app
+connectMongoDB() // connect to MongoDB
+
+async function connectMongoDB () {
+    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, });
+    const db = mongoose.connection;
+    db.once('open', () => console.log('Connected to MongoDB') );
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(port = 3000, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+    mongoUri: process.env.MONGO_URI,
+    dbName: "calendar_events",
+  };
