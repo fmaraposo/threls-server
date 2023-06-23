2e963a4405358066423d7dba065e5d00c82e6914
Francisco Martins <fmaraposo@gmail.com>
2023-06-23 09:00:14 +0100

    add body parser middleware

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { mongoUri } = require("./config");
+const express = require('express')
+const mongoose = require('mongoose')
+const { mongoUri } = require('./config')
+const eventsRouter = require('./routes/eventsRouter')
 
 const app = express() // start express app
+app.use(express.json()) // body parser middleware
 connectMongoDB() // connect to MongoDB
 
 async function connectMongoDB() {
-    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, });
-    const db = mongoose.connection;
-    db.once('open', () => console.log('Connected to MongoDB'));
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+	mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+	const db = mongoose.connection
+	db.once('open', () => console.log('Connected to MongoDB'))
+	db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 }
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use('/api', eventsRouter);
+
+app.listen((port = 3000), () => {
+	console.log(`Server is running on port ${port}`)
 })
-
-
-app.listen(port = 3000, () => {
-    console.log(`Server is running on port ${port}`);
-});
