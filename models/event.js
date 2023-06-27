6faf4a2260eb73f64f413a740fbdd81b31916a5b
Francisco Martins <fmaraposo@gmail.com>
2023-06-27 10:06:32 +0100

    logic to check if events are overlapping

--- models/event.js
+++ models/event.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 const eventSchema = new mongoose.Schema({
-	id: { type: String, required: true },
 	title: { type: String, required: true },
 	startDate: { type: Date, required: true },
 	endDate: { type: Date, required: true },
--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -1,23 +1,43 @@
 const express = require('express')
 const router = express.Router()
 const Event = require('../models/event')
-const { randomUUID } = require('crypto')
+const { areIntervalsOverlapping } = require('date-fns')
+
+const isOverlapping = async (event, edit) => {
+	let existingEvents
+	if (edit) {
+		// Find all events except the one being edited
+		existingEvents = await Event.find({ _id: { $ne: event._id } })
+	} else {
+		// Find all events except the one being edited
+		existingEvents = await Event.find()
+	}
+
+	const isOverlap = existingEvents.some((e) => {
+		// Check if the event overlaps with any existing event
+		const eventOverlap = areIntervalsOverlapping(
+			{ start: new Date(e.startDate), end: new Date(e.endDate) },
+			{ start: new Date(event.startDate), end: new Date(event.endDate) }
+		)
+
+		if (eventOverlap) return true
+		else return false
+	})
+
+	return isOverlap
+}
 
 // Get all events that start from a particular date
-router.get('/:date', async (req, res) => {
+router.get('/:date/:endDate', async (req, res) => {
 	try {
-		const { date } = req.params
+		const { date, endDate } = req.params
 
-		// Convert the date string to a JavaScript Date object
-		const searchDate = new Date(date)
-
-		// Find all events that occur on or after the specified date
-		const events = await Event.find({ startDate: { $gte: searchDate } })
+		// Find all events that occur on the specified date
+		const events = await Event.find({ startDate: { $gte: date, $lte: endDate } })
 
 		res.json(events)
 	} catch (error) {
-		console.error(error)
-		res.status(500).json({ message: 'Server Error' })
+		res.status(500).json({ error: 'Server Error', message: error.message })
 	}
 })
 
@@ -25,11 +45,48 @@ router.get('/:date', async (req, res) => {
 router.post('/saveEvent', async (req, res) => {
 	try {
 		const eventData = req.body
-		const event = new Event({ ...eventData, id: randomUUID() })
-		const savedEvent = await event.save()
-		res.json(savedEvent)
+		const event = new Event(eventData)
+		if (await isOverlapping(eventData)) {
+			res.json({ error: 'Event overlaps with an existing event.' })
+		} else {
+			const savedEvent = await event.save()
+			res.json({ event: savedEvent, message: 'Event saved successfully.' })
+		}
 	} catch (error) {
-		res.status(500).json({ error: 'An error occurred while saving the event.' })
+		res
+			.status(500)
+			.json({ error: 'An error occurred while saving the event.', message: error.message })
+	}
+})
+
+// Delete an event
+router.delete('/deleteEvent/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		await Event.deleteOne({ id })
+		res.json({ message: 'Event deleted successfully.' })
+	} catch (error) {
+		res
+			.status(500)
+			.json({ error: 'An error occurred while deleting the event.', message: error.message })
+	}
+})
+
+// Update an event
+router.put('/updateEvent/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const eventData = req.body
+		if (await isOverlapping({ ...eventData, _id: id }, true)) {
+			res.json({ error: 'Event overlaps with an existing event.' })
+		} else {
+			await Event.updateOne({ _id: id }, eventData)
+			res.json({ message: 'Event updated successfully.', event: { ...eventData, _id: id } })
+		}
+	} catch (error) {
+		res
+			.status(500)
+			.json({ error: 'An error occurred while updating the event.', message: error.message })
 	}
 })
 
