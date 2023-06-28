4552e8152c898067b5a2df78ce5b1853d08deda6
Francisco Martins <fmaraposo@gmail.com>
2023-06-28 22:49:22 +0100

    update  script to check if event is overlapping to increase performance

--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const Event = require('../models/event')
-const { areIntervalsOverlapping, isSameDay } = require('date-fns')
+const { isSameDay } = require('date-fns')
 
 const isOverlapping = async (event, edit) => {
 	let existingEvents
 	if (edit) {
-		// Find all events except the one being edited
-		existingEvents = await Event.find({ _id: { $ne: event._id } })
+		existingEvents = overlappingEvents = await Event.find({
+			_id: { $ne: event._id }, // Exclude the updated event itself
+			$or: [
+				{ startDate: { $lt: event.endDate }, endDate: { $gt: event.startDate } },
+				{ startDate: { $eq: event.startDate }, endDate: { $eq: event.endDate } },
+				{ startDate: { $gte: event.startDate, $lt: event.endDate } },
+				{ endDate: { $gt: event.startDate, $lte: event.endDate } },
+			],
+		})
 	} else {
-		// Find all events except the one being edited
-		existingEvents = await Event.find()
+		existingEvents = await Event.find({
+			$or: [
+				{ startDate: { $lt: event.endDate }, endDate: { $gt: event.startDate } },
+				{ startDate: { $eq: event.startDate }, endDate: { $eq: event.endDate } },
+				{ startDate: { $gte: event.startDate, $lt: event.endDate } },
+				{ endDate: { $gt: event.startDate, $lte: event.endDate } },
+			],
+		})
 	}
 
-	const isOverlap = existingEvents.some((e) => {
-		// Check if the event overlaps with any existing event
-		const eventOverlap = areIntervalsOverlapping(
-			{ start: new Date(e.startDate), end: new Date(e.endDate) },
-			{ start: new Date(event.startDate), end: new Date(event.endDate) }			
-		)
-
-		if (eventOverlap) return true
-		else return false
-	})
-
-	return isOverlap
+	return existingEvents.length > 0
 }
 
 const areDatesOnSameDay = (startDate, endDate) => {
